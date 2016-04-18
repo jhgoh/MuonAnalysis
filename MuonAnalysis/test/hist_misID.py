88be2a0b70a2a5a403f44d2856efb3ff773bcf2a
Junghwan John Goh <jhgoh@cern.ch>
2016-04-18 16:22:21 +0200

    use event list

--- MuonAnalysis/test/hist_misID.py
+++ MuonAnalysis/test/hist_misID.py
@@ -22,68 +22,104 @@ idSet = {
     "TMLastAngLoose":"isTMLastAngLoose", "TMLastAngTight":"isTMLastAngTight", "TMOneAngLoose":"isTMOneAngLoose", "TMOneAngTight":"isTMOneAngTight",
 }
 
+def makedirs(d, path):
+    for p in path.split('/'):
+        dd = d.GetDirectory(p)
+        if dd == None: dd = d.mkdir(p)
+        d = dd
+    return d
+
 def project(*args):
-    dirName, fName = args[0]
+    dirName, mode, fName = args[0]
     print "@@ Processing", fName
+
+    ptbins = modeSet[mode]["ptbins"]
+    aetabins = modeSet[mode]["aetabins"]
+    nbins, minMass, maxMass = modeSet[mode]["massbin"]
+    binW = (maxMass-minMass)/nbins
+
     if fName.startswith("root://"): f = TNetXNGFile(fName)
     else: f = TFile(fName)
-  
     if f == None or f.IsZombie(): return
+    tree = f.Get("%s/tree" % mode)
 
-    fout = TFile("%s/%s" % (dirName, os.path.basename(fName)), "RECREATE")
+    fout = TFile("%s/%s/%s" % (dirName, mode, os.path.basename(fName)), "RECREATE")
     fout.cd()
 
-    for mode in modeSet:
-        ptbins = modeSet[mode]["ptbins"]
-        aetabins = modeSet[mode]["aetabins"]
-        nbins, minMass, maxMass = modeSet[mode]["massbin"]
+    eventList = TEventList("eventList");
+    bins = []
+    for ptbin in range(len(ptbins)-1):
+        minPt, maxPt = ptbins[i], ptbins[i+1]
+        for leg in range(2):
+            cutBin = "trk_pt[%d] >= %f && trk_pt[%d] < %f" % (leg, minPt, leg, maxPt)
+            print "@@@@ Building event list", os.path.basename(fName), "ptbin", ptbin, "...",
+            tree.Draw(">>eventList", "(%s) && (%s)" % (precut, cutBin))
+            print "done"
 
-        tree = f.Get("%s/tree" % mode)
-        modeDir = fout.mkdir(mode)
-        for idName in idSet:
-            print "@@ Processing", mode, idName
-            idDir = modeDir.mkdir(idName)
-            cutID1 = "mu_dR[0]<0.01 && mu_%s[0]" % idSet[idName]
-            cutID2 = "mu_dR[1]<0.01 && mu_%s[1]" % idSet[idName]
+            for idName in idSet:
+                cutID = "mu_dR[%d]<0.01 && mu_%s[%d]" % (leg, idSet[idName], leg)
 
-            for i in range(len(ptbins)-1):
-                minPt, maxPt = ptbins[i], ptbins[i+1]
-                for j in range(len(aetabins)-1):
-                    binDir1 = idDir.mkdir("leg1_bin_pt%d_aeta%d" % (i, j))
-                    binDir2 = idDir.mkdir("leg2_bin_pt%d_aeta%d" % (i, j))
+                outdir = makedirs(fout, "%s/%s/leg%d_ptbin%d" % (mode, idName, leg+1, ptbin))
+                outdir.cd()
 
-                    minAeta, maxAeta = aetabins[j], aetabins[j+1]
-                    cutBin1 = "trk_pt[0] >= %f && trk_pt[0] < %f && fabs(trk_eta[0]) >= %f && fabs(trk_eta[0]) < %f" % (minPt, maxPt, minAeta, maxAeta)
-                    cutBin2 = "trk_pt[1] >= %f && trk_pt[1] < %f && fabs(trk_eta[1]) >= %f && fabs(trk_eta[1]) < %f" % (minPt, maxPt, minAeta, maxAeta)
+                cutStrObjPass = TObjString("cutPass")
+                cutStrObjFail = TObjString("cutFail")
+                cutStrObjPass.SetString("(%s) && (%s) &&  (%s)" % (precut, cutBin, cutID))
+                cutStrObjFail.SetString("(%s) && (%s) && !(%s)" % (precut, cutBin, cutID))
+                cutStrObjPass.Write()
+                cutStrObjFail.Write()
 
-                    binDir1.cd()
-                    hPass1 = TH1D("hPass", "Passing;Mass (GeV);Candidates per %f MeV", nbins, minMass, maxMass)
-                    hFail1 = TH1D("hFail", "Failing;Mass (GeV);Candidates per %f MeV", nbins, minMass, maxMass)
-                    tree.Draw("vtx_mass>>hPass", "(%s) && (%s) &&  (%s)" % (precut, cutBin1, cutID1), "goff")
-                    tree.Draw("vtx_mass>>hFail", "(%s) && (%s) && !(%s)" % (precut, cutBin1, cutID1), "goff")
-                    hPass1.Write()
-                    hFail1.Write()
+                hPass = TH1D("hPass", "Passing;Mass (GeV);Candidates per %f MeV" % binW, nbins, minMass, maxMass)
+                hFail = TH1D("hFail", "Failing;Mass (GeV);Candidates per %f MeV" % binW, nbins, minMass, maxMass)
+                tree.Draw("vtx_mass>>hPass", " (%s)" % cutID, "goff")
+                tree.Draw("vtx_mass>>hFail", "!(%s)" % cutID, "goff")
+                hPass.Write()
+                hFail.Write()
 
-                    binDir2.cd()
-                    hPass2 = TH1D("hPass", "Passing;Mass (GeV);Candidates per %f MeV", nbins, minMass, maxMass)
-                    hFail2 = TH1D("hFail", "Failing;Mass (GeV);Candidates per %f MeV", nbins, minMass, maxMass)
-                    tree.Draw("vtx_mass>>hPass", "(%s) && (%s) &&  (%s)" % (precut, cutBin2, cutID2), "goff")
-                    tree.Draw("vtx_mass>>hFail", "(%s) && (%s) && !(%s)" % (precut, cutBin2, cutID2), "goff")
-                    hPass2.Write()
-                    hFail2.Write()
+    for aetabin in range(len(aetabins)-1):
+        print "@@@@ ", os.path.basename(fName), "aetabin", aetabin
+        minAeta, maxAeta = aetabins[aetabin], aetabins[aetabin+1]
+        for leg in range(2):
+            print "@@@@ Building event list", os.path.basename(fName), "aetabin", aetabin, "...",
+            cutBin = "fabs(trk_eta[%d]) >= %f && fabs(trk_eta[%d]) < %f" % (leg, minAeta, leg, maxAeta)
+            tree.Draw(">>eventList", "(%s) && (%s)" % (precut, cutBin))
+            print "done"
+
+            for idName in idSet:
+                cutID = "mu_dR[%d]<0.01 && mu_%s[%d]" % (leg, idSet[idName], leg)
+
+                outdir = makedirs(fout, "%s/%s/leg%d_aetabin%d" % (mode, idName, leg+1, aetabin))
+                outdir.cd()
+
+                cutStrObjPass = TObjString("cutPass")
+                cutStrObjFail = TObjString("cutFail")
+                cutStrObjPass.SetString("(%s) && (%s) &&  (%s)" % (precut, cutBin, cutID))
+                cutStrObjFail.SetString("(%s) && (%s) && !(%s)" % (precut, cutBin, cutID))
+                cutStrObjPass.Write()
+                cutStrObjFail.Write()
+
+                hPass = TH1D("hPass", "Passing;Mass (GeV);Candidates per %f MeV" % binW, nbins, minMass, maxMass)
+                hFail = TH1D("hFail", "Failing;Mass (GeV);Candidates per %f MeV" % binW, nbins, minMass, maxMass)
+                tree.Draw("vtx_mass>>hPass", " (%s)" % cutID, "goff")
+                tree.Draw("vtx_mass>>hFail", "!(%s)" % cutID, "goff")
+                hPass.Write()
+                hFail.Write()
+
     fout.Write()
     print "@@ Finished", fName
 
 if __name__ == '__main__':
-    if not os.path.exists("RD"): os.mkdir("RD")
-    if not os.path.exists("MC"): os.mkdir("MC")
 
+    #eosBase = "/afs/cern.ch/user/j/jhgoh/eos/cms/store/user/jhgoh/MuonMisID/20160412_1"
     eosBase = "root://eoscms//eos/cms/store/user/jhgoh/MuonMisID/20160412_1"
-    filesRD = ['%s/JetHT_2015D/JetHT/crab_20160412_152954/160412_133018/0000/hist_%d.root' % (eosBase, i) for i in range(1,162)]
-    filesMC = ['%s/TT_powheg/TT_TuneCUETP8M1_13TeV-powheg-pythia8/crab_20160412_153031/160412_133052/0000/hist_%d.root' % (eosBase, i) for i in range(1, 629)]
+    filesRD = ['%s/JetHT_2015D/JetHT/crab_20160412_152954/160412_133018/0000/hist_%d.root' % (eosBase, i) for i in range(1,2)]#,162)]
+    filesMC = ['%s/TT_powheg/TT_TuneCUETP8M1_13TeV-powheg-pythia8/crab_20160412_153031/160412_133052/0000/hist_%d.root' % (eosBase, i) for i in range(1, 2)]#629)]
 
     p = Pool(processes = 8)
-    p.map(project, zip(["RD"]*len(filesRD), filesRD))
+    for mode in modeSet:
+        if not os.path.exists("RD/%s" % mode): os.makedirs("RD/%s" % mode)
+        if not os.path.exists("MC/%s" % mode): os.makedirs("MC/%s" % mode)
 
-    p.map(project, zip(["MC"]*len(filesMC), filesMC))
+        p.map(project, zip(["RD"]*len(filesRD), [mode]*len(filesRD), filesRD))
+        p.map(project, zip(["MC"]*len(filesMC), [mode]*len(filesMC), filesMC))
 
