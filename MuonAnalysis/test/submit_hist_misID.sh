f0d2123b6599290b2fbf1e11b40922325d055d4c
Junghwan John Goh <jhgoh@cern.ch>
2016-04-19 12:00:38 +0200

    refine submit queue

--- MuonAnalysis/test/submit_hist_misID.sh
+++ MuonAnalysis/test/submit_hist_misID.sh
@@ -11,17 +11,15 @@ if [ ! -d MC ]; then
   mkdir -p MC/lamb
 fi
 
-SUBMIT='bsub -q 1nh'
-
 for i in `seq 1 161`; do 
-    $SUBMIT -oo RD/ks/log_$i.log run_hist_misID.sh RD ks $i
-    $SUBMIT -oo RD/phi/log_$i.log run_hist_misID.sh RD phi $i
-    $SUBMIT -oo RD/lamb/log_$i.log run_hist_misID.sh RD lamb $i
+    bsub -q 1nh -oo RD/ks/log_$i.log run_hist_misID.sh RD ks $i
+    bsub -q 8nh -oo RD/phi/log_$i.log run_hist_misID.sh RD phi $i
+    bsub -q 1nh -oo RD/lamb/log_$i.log run_hist_misID.sh RD lamb $i
 done
 
 for i in `seq 1 628`; do 
-    $SUBMIT -oo MC/ks/log_$i.log run_hist_misID.sh MC ks $i
-    $SUBMIT -oo MC/phi/log_$i.log run_hist_misID.sh MC phi $i
-    $SUBMIT -oo MC/lamb/log_$i.log run_hist_misID.sh MC lamb $i
+    bsub -q 1nh -oo MC/ks/log_$i.log run_hist_misID.sh MC ks $i
+    bsub -q 8nh -oo MC/phi/log_$i.log run_hist_misID.sh MC phi $i
+    bsub -q 1nh -oo MC/lamb/log_$i.log run_hist_misID.sh MC lamb $i
 done
 
