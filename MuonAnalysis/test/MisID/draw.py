c021218dd2233494babd3cdd1512b5fd95e9a6df
Junghwan John Goh <jhgoh@cern.ch>
2016-05-17 14:59:16 +0200

    automatic scale

--- MuonAnalysis/test/MisID/draw.py
+++ MuonAnalysis/test/MisID/draw.py
@@ -17,6 +17,7 @@ if len(sys.argv) < 3 or \
 dataType = sys.argv[1]
 submod = sys.argv[2]
 
+from math import *
 from ROOT import *
 from SKKU.CommonTools.tdrStyle import *
 tdrStyle.SetTitleSize(0.055, "XYZ")
@@ -30,22 +31,29 @@ varNames = {
     "pt":("%s transverse momentum p_{T} (GeV)" % xtitle),
     "abseta":("%s pseudorapidity |#eta|" % xtitle)
 }
-idNames = [
-    ("RPC","RPC Loose ID"),
-    ("TRK","Tracker muons (no arbitration)"),
-    ("Loose","Loose muon ID"),
-    ("Medium","Medium muon ID"),
-    ("Tight","Tight muon ID"),
-    ("Soft","Soft muon ID"),
+idSets = [
+    ("RPC","RPC Loose ID", kRed),
+    ("TRK","Tracker muons (no arbitration)", kBlue),
+    ("Loose","Loose muon ID", kGreen+1),
+    ("Medium","Medium muon ID", kMagenta),
+    ("Tight","Tight muon ID", kBlack),
+    ("Soft","Soft muon ID", kAzure+1),
+    #("OneLoose","TMOneStationLoose", kBlue),
+    #("OneTight","TMOneStationTight", kGreen+1),
+    #("TMLastLoose","TMLastStationLoose", kGreen+1),
+    #("TMLastTight","TMLastStationTight", kMagenta),
+    #("TM2DLoose","TM2DLoose" kBlack),
+    #("TM2DTight","TM2DTight", kAzure+1),
 ]
-colors = [kRed, kBlue, kGreen+1, kMagenta, kBlack, kAzure+1]
 
 frames = {}
 graphs = {}
 
+maxPt = 20
+maxY = 0
 f = TFile("%s/fit_%s.root" % (dataType, mode))
 #for idName in [x.GetName() for x in f.GetDirectory(submod).GetListOfKeys()]:
-for i, (idName, idTitle) in enumerate(idNames):
+for i, (idName, idTitle, color) in enumerate(idSets):
     idDir = f.GetDirectory("%s/%s" % (submod, idName))
     if idDir == None: continue
     for varName in [x.GetName() for x in idDir.GetListOfKeys()]:
@@ -58,26 +66,33 @@ for i, (idName, idTitle) in enumerate(idNames):
             graphs[varName] = []
         g = varDir.Get("gRatio")
         g.SetTitle(idTitle)
-        g.SetMarkerColor(colors[i])
-        g.SetLineColor(colors[i])
+        g.SetMarkerColor(color)
+        g.SetLineColor(color)
         for p in xrange(g.GetN()):
             g.SetPoint(p, g.GetX()[p], g.GetY()[p]*100)
             g.SetPointError(p, g.GetEXlow()[p], g.GetEXhigh()[p], g.GetEYlow()[p]*100, g.GetEYhigh()[p]*100)
+            maxYatP =  g.GetY()[p]+g.GetEYhigh()[p]
+            maxXatP = g.GetX()[p]+g.GetEXhigh()[p]
+            if varName != "pt" or maxXatP < maxPt*1.001:
+                if (varName != "pt" and p < g.GetN()-1) or \
+                   (varName == "pt" and p < frame.FindBin(maxPt*0.999)-1):
+                    maxYatP /= 0.85-0.04*len(idSets)
+                maxY = max(maxY, maxYatP)
         graphs[varName].append(g)
+maxY = ceil(maxY*10)/10
 
 objs = []
 for varName in frames:
     c = TCanvas("c%s" % varName, varName, 500, 500)
     c.cd()
 
-    leg = TLegend(0.2, 0.85-0.04*len(idNames), 0.7, 0.85)
+    leg = TLegend(0.2, 0.85-0.04*len(idSets), 0.7, 0.85)
     leg.SetFillStyle(0)
     leg.SetBorderSize(0)
     frame = frames[varName]
 
-    ranges = [frame.GetXaxis().GetXmin(), frame.GetXaxis().GetXmax(), 0, 4.0]
-    if submod in ("Kp", "Km"): ranges[-1] = 7.0
-    if varName == "pt": ranges[1] = 20
+    ranges = [frame.GetXaxis().GetXmin(), frame.GetXaxis().GetXmax(), 0, maxY]
+    if varName == "pt": ranges[1] = maxPt
     frame.GetXaxis().SetRangeUser(ranges[0],ranges[1])
     frame.SetMinimum(ranges[2])
     frame.SetMaximum(ranges[3])
@@ -97,7 +112,7 @@ for varName in frames:
     label.SetTextAlign(11)
     label.SetMargin(0)
     label.AddText("CMS 2015 work in progress #sqrt{s} = 13 TeV, L=2.XY fb^{-1}")
-    label.Draw()
+    #label.Draw()
 
     c.Print("c_%s_%s_%s.png" % (submod, varName, dataType))
 
