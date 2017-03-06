1272dd88ac2e4ec8a6f3cf6738ef871a8bc8ea55
Junghwan John Goh <jhgoh@cern.ch>
2017-03-06 07:23:54 +0100

    minor crabConfig updates

--- test/MisID/crabConfigMC.py
+++ test/MisID/crabConfigMC.py
@@ -18,16 +18,15 @@ config.section_("Site")
 # Where the output files will be transmitted to
 config.Site.storageSite = 'T2_CH_CERN'
 
-dateTrial = "20170306_1"
-config.General.workArea = "MuonMisID/TT_powheg"
+## Configurations that can change
+dateTrial = "20170306_2"
 config.Data.inputDataset = "/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIIFall15DR76-PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext4-v1/AODSIM"
-config.Data.outLFNDirBase = '/store/user/jhgoh/MuonMisID/%s/TT_powheg' % dateTrial
-config.General.requestName = "MuonMisID_TT_powheg_%s" % dateTrial
-
 import os
 if 'DATASET' in os.environ:
     config.Data.inputDataset = os.environ['DATASET']
-    config.Data.outLFNDirBase = os.environ['OUTLFN']
-    config.General.workArea = "MuonMisID/%s" % (os.environ['OUTLFN'].split('/')[-1])
-    config.General.requestName = "MuonMisID_%s_%s" % (os.environ['OUTLFN'].split('/')[-1], dateTrial)
+pd = config.Data.inputDataset.split('/')[1].replace('_TuneCUETP8M1_13TeV_pythia8', '')
+
+config.General.workArea = "%s/%s" % (dateTrial, pd)
+config.General.requestName = "MuonMisID_%s_%s" % (dateTrial, pd)
+config.Data.outLFNDirBase = '/store/user/jhgoh/MuonMisID/%s/%s' % (dateTrial, pd)
 
--- test/MisID/crabConfigRD.py
+++ test/MisID/crabConfigRD.py
@@ -11,20 +11,30 @@ config.JobType.psetName    = 'prod_RD_cfg.py'
 
 config.section_("Data")
 config.Data.publication  = False
-config.Data.inputDataset = '/JetHT/Run2015D-16Dec2015-v1/AOD'
 config.Data.splitting = "LumiBased"
 
-config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_v2.txt'
 config.Data.unitsPerJob = 200
 
 config.section_("Site")
 # Where the output files will be transmitted to
 config.Site.storageSite = 'T2_CH_CERN'
 
-dateTrial = "20170306_1"
-config.General.workArea = "MuonMisID/JetHT_2015D"
-config.Data.outLFNDirBase = '/store/user/jhgoh/MuonMisID/%s/JetHT_2015D' % dateTrial
-config.General.requestName = "MuonMisID_JetHT_2015D_%s" % dateTrial
+## Configurations that can change
+dateTrial = "20170306_2"
+config.Data.inputDataset = '/JetHT/Run2015D-16Dec2015-v1/AOD'
+import os
+if 'DATASET' in os.environ:
+    config.Data.inputDataset = os.environ['DATASET']
+pd, sd = config.Data.inputDataset.split('/')[1:3]
+lumiMaskBase = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/'
+if '2015' in sd:
+    config.Data.lumiMask = lumiMaskBase+'Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_v2.txt'
+elif '2016' in sd:
+     config.Data.lumiMask = lumiMaskBase+'Collisions16/13TeV/Final/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'
+
+config.General.workArea = "%s/%s_%s" % (dateTrial, pd, sd)
+config.General.requestName = "MuonMisID_%s_%s_%s" % (dateTrial, pd, sd)
+config.Data.outLFNDirBase = '/store/user/jhgoh/MuonMisID/%s/%s_%s' % (dateTrial, pd, sd)
 
 #config.Data.lumiMask = 'notFinishedLumis.json'
 #config.Data.unitsPerJob = 1
